66df1f0dafd026d918bcd9e47f61f4a96fee1ab1
Michael Campbell <campbelltm15@gmail.com>
2025-02-11 20:36:31 -0500

    Add project files.

---
+++ QueuesAssignment1/Main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+
+using namespace std;
+
+struct User
+{
+	string username;
+	int level;
+};
+
+
+struct LinkedNode
+{
+	User user;
+	Queue* Next;
+};
+
+
+struct Queue
+{
+	Queue* Head;
+	Queue* Tail;
+};
+
+
+int main()
+{
+
+
+	
+}
